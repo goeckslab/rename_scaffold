eb7c6866b218a41d9b22255ee6afb83551a5ff40
Yating Liu <yatinghust@gmail.com>
2018-06-25 12:27:19 -0700

    * remove header from name mapping file * update tool description

--- rename.py
+++ rename.py
@@ -3,10 +3,8 @@ Call rename to rename scaffolds in reference genome so that the sequence names a
 """
 import sys
 import csv
-from collections import OrderedDict
 
 def rename(inputfile, outputfile, writer):
-    namemap = OrderedDict()
     with open(outputfile, 'w') as out:
         with open(inputfile, 'r') as rf:
             lines = rf.readlines()
@@ -15,20 +13,17 @@ def rename(inputfile, outputfile, writer):
                 if ">" in line:
                     oldname = line[1:].rstrip()
                     newname = "scaffold_" + str(i)
-                    line = ">" + newname
+                    line = ">" + newname + "\n"
                     i = i+1
                     writer.writerow([oldname, newname])
-                #TODO: Add line breaks to chromosome sequences that are in a single line
-                out.write(line.rstrip() + "\n")
+                out.write(line)
 
 def main():
     inputfile = str(sys.argv[1])
     outputfile = str(sys.argv[2])
     indexfile = str(sys.argv[3])
     csvfile = open(indexfile, 'w')
-    fieldnames = ['Original sequence name', 'Renamed sequence name']
     writer = csv.writer(csvfile)
-    writer.writerow(fieldnames)
     rename(inputfile, outputfile, writer)
 
 if __name__ == "__main__":
