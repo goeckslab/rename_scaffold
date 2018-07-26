ad10b2928b666b2c0a50df94ba94a17c2a78dad9
Yating Liu <yatinghust@gmail.com>
2018-07-26 08:15:38 -0700

    When calling truncate, replace non-ASCII character with '_'.

--- rename.py
+++ rename.py
@@ -1,10 +1,11 @@
 """
 Call rename to rename scaffolds in reference genome so that the sequence names are less than 31 characters. Rename all scaffolds to scaffold_1, scaffold_2, ..., scaffold_N and provide a name mapping file
-Call truncate to truncate the scaffold names that are more than 31 characters.
+Call truncate to truncate the scaffold names that are more than 31 characters. Replace non-ASCII character with '_'
 """
 import sys
 import csv
 import codecs
+import string
 
 def rename(inputfile, outputfile, writer):
     with open(outputfile, 'w') as out:
@@ -26,12 +27,24 @@ def truncate(inputFile, outputFile):
             lines = rf.readlines()
             for l in lines:
                 if ">" in l:
+                    print l.encode('utf-8')
                     name = l[1:].rstrip()
+                    name = substituteNonAscii(name)
                     if len(name) > 31:
-                        print "truncate " + name.encode('utf-8') + " to less than 31 characters"
                         name = name[:31]
-                        l = ">" + name + "\n"
+                        print "\tTruncate the scaffold name to less than 31 characters: %s" % name
+                    l = ">" + name + "\n"
+                    print "======================\n"
                 out.write(l)
+
+def substituteNonAscii(str):
+    l = []
+    for c in str:
+        if c not in string.printable:
+            print "\tSubstitute Non-ASCII character %s with _" % c.encode('utf-8')
+            c = '_'
+        l.append(c)
+    return "".join(l)
 
 def main():
     inputfile = str(sys.argv[1])
